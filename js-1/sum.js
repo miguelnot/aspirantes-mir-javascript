6e7ab10c4150dfd47cac0ad4f5cb4833189929ee
miguel <miguelnothing@gmail.com>
2023-05-10 20:19:08 -0500

    resolucion de los ejercicios java

---
+++ js-1/sum.js
@@ -0,0 +1,10 @@
+// escribe la función suma acá
+    function sum (n) {
+        return n * (n+1) /2
+
+    }
+
+// código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
